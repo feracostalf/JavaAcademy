81ef0e012e84c2259b27435e0791b148e37583b2
feracostalf <43489983+feracostalf@users.noreply.github.com>
2022-04-16 21:42:57 -0500

    SortedArray

---
+++ Collections/Collections/src/examples/MyArrays.java
@@ -0,0 +1,15 @@
+package examples;
+
+import java.util.Arrays;
+
+public class MyArrays {
+    public static void main(String[] args) {
+        String[] array= new String[5];
+
+        array[0]="IBM";
+        array[1]="JAVA";
+        array[2]="ACADEMY";
+
+        System.out.println(Arrays.toString(array));
+    }
+}
---
+++ Collections/Collections/src/test/challenges/sortarrays/SortMyArrays.java
@@ -0,0 +1,41 @@
+package test.challenges.sortarrays;
+
+import java.util.Arrays;
+
+public class SortMyArrays {
+    public static void main(String[] args) {
+
+        int[] numbers= new int[]{6,3,1,2,7,5,4,8,9};
+
+        //Arrays.sort(numbers);
+
+        sortArray(numbers);
+        printArray(numbers);
+        
+    }
+
+    public static void sortArray(int[] numbers){
+        int idxAux=0;
+        int numAux1=0;
+        int numAux2=0;
+        for(int i=0; i<numbers.length; i++){
+            idxAux=i;
+            for(int j=i+1; j<numbers.length-1; j++){              
+                if(numbers[j]<numbers[idxAux]){
+                    idxAux=j;
+                }
+            }
+            numAux1=numbers[i];
+            numAux2=numbers[idxAux];
+            numbers[idxAux]=numAux1;
+            numbers[i]=numAux2;
+        }
+    }
+
+public static void printArray(int[] myArray){
+    for(int e:myArray){
+        System.out.print(e);
+    }
+}
+
+}
